964cb360061cb3793d8d8c234f61f58b70781c1d
d4lion <d4lion.dev@gmail.com>
2025-02-26 19:49:13 -0500

    Add database management functionality with SQLite and interface definitions

---
+++ src/Database.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+
+class Context:
+    def __init__(self) -> None:
+        self.connection = sqlite3.connect("data.db", check_same_thread=False)
+
+    def get_all_users(self):
+        cursor = self.connection.cursor()
+        query = "SELECT * FROM users"
+
+        cursor.execute(query)
+        return cursor.fetchall()
+
+    def create_new_user(self, id, name, email, phone, last_name=None):
+        cursor = self.connection.cursor()
+        query = """INSERT INTO users (id, name, last_name, email, phone)
+                VALUES (?, ?, ?, ?, ?)"""
+
+        cursor.execute(query, (id, name, last_name, email, phone))
+        self.connection.commit()
+
+    def delete_user(self, id: int):
+        cursor = self.connection.cursor()
+        query = "DELETE FROM users WHERE id = ?"
+
+        cursor.execute(query, (id,))
+        self.connection.commit()
---
+++ src/page/interfaces/IDatabaseContext.py
@@ -0,0 +1,12 @@
+class Context:
+    def __init__(self) -> None:
+        pass
+
+    def get_all_users(self):
+        pass
+
+    def create_new_user(self, id, name, email, phone, last_name=None):
+        pass
+
+    def delete_user(self, id):
+        pass
